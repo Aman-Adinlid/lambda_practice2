72e61057f2c30a382d9d92fe0e92db7399f2da59
aman adinlid <95pinkpanda@gmail.com>
2021-02-03 10:49:12 +0100

    test part

--- src/main/java/se/lexicon/Exercises.java
+++ src/main/java/se/lexicon/Exercises.java
@@ -1,8 +1,10 @@
 package se.lexicon;
 
 import se.lexicon.data.DataStorage;
+import se.lexicon.model.Gender;
 import se.lexicon.model.Person;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.function.Predicate;
 
@@ -28,6 +30,13 @@ public class Exercises {
      */
     public static void exercise2(String message){
         System.out.println(message);
+        Predicate<Person> findALLFemale = person -> person.getGender()== Gender.FEMALE;
+        List<Person> females = storage.findMany(findALLFemale);
+        females.forEach(person -> System.out.println(person));
+
+
+
+
         //Write your code here
 
         System.out.println("----------------------");
